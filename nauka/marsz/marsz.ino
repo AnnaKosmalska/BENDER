c454b0ac19759b9ac1b252772e0a2e9e1cbbbbce
Kosmal <mariusz.kosmalski@gmail.com>
2017-11-18 17:06:07 +0100

    motor initial

--- nauka/marsz/marsz.ino
+++ nauka/marsz/marsz.ino
@@ -34,126 +34,126 @@ byte out[9];
 
 bool light;
 bool off;
-int speakerPin = 13;
+int speakerPin = 10;
 void beep (unsigned char speakerPin, int frequencyInHertz, long timeInMilliseconds)
-{ 
-    //digitalWrite(ledPin, HIGH);   
-    //use led to visualize the notes being played
-    
-    int x;   
-    long delayAmount = (long)(1000000/frequencyInHertz);
-    long loopTime = (long)((timeInMilliseconds*1000)/(delayAmount*2));
-    for (x=0;x<loopTime;x++)   
-    {    
-        digitalWrite(speakerPin,HIGH);
-        delayMicroseconds(delayAmount);
-        digitalWrite(speakerPin,LOW);
-        delayMicroseconds(delayAmount);
-    }    
-    
-    //digitalWrite(ledPin, LOW);
-    //set led back to low
-    
-    delay(20);
-    //a little delay to make all notes sound separate
-} 
+{
+  //digitalWrite(ledPin, HIGH);
+  //use led to visualize the notes being played
+
+  int x;
+  long delayAmount = (long)(1000000 / frequencyInHertz);
+  long loopTime = (long)((timeInMilliseconds * 1000) / (delayAmount * 2));
+  for (x = 0; x < loopTime; x++)
+  {
+    digitalWrite(speakerPin, HIGH);
+    delayMicroseconds(delayAmount);
+    digitalWrite(speakerPin, LOW);
+    delayMicroseconds(delayAmount);
+  }
+
+  //digitalWrite(ledPin, LOW);
+  //set led back to low
+
+  delay(20);
+  //a little delay to make all notes sound separate
+}
 void march()
-{    
-    //for the sheet music see:
-    //http://www.musicnotes.com/sheetmusic/mtd.asp?ppn=MN0016254
-    //this is just a translation of said sheet music to frequencies / time in ms
-    //used 500 ms for a quart note
-    
-    beep(speakerPin, a, 500); 
-    beep(speakerPin, a, 500);     
-    beep(speakerPin, a, 500); 
-    beep(speakerPin, f, 350); 
-    beep(speakerPin, cH, 150);
-    
-    beep(speakerPin, a, 500);
-    beep(speakerPin, f, 350);
-    beep(speakerPin, cH, 150);
-    beep(speakerPin, a, 1000);
-    //first bit
-    
-    beep(speakerPin, eH, 500);
-    beep(speakerPin, eH, 500);
-    beep(speakerPin, eH, 500);    
-    beep(speakerPin, fH, 350); 
-    beep(speakerPin, cH, 150);
-    
-    beep(speakerPin, gS, 500);
-    beep(speakerPin, f, 350);
-    beep(speakerPin, cH, 150);
-    beep(speakerPin, a, 1000);
-    //second bit...
-    
-    beep(speakerPin, aH, 500);
-    beep(speakerPin, a, 350); 
-    beep(speakerPin, a, 150);
-    beep(speakerPin, aH, 500);
-    beep(speakerPin, gSH, 250); 
-    beep(speakerPin, gH, 250);
-    
-    beep(speakerPin, fSH, 125);
-    beep(speakerPin, fH, 125);    
-    beep(speakerPin, fSH, 250);
-    delay(250);
-    beep(speakerPin, aS, 250);    
-    beep(speakerPin, dSH, 500);  
-    beep(speakerPin, dH, 250);  
-    beep(speakerPin, cSH, 250);  
-    //start of the interesting bit
-    
-    beep(speakerPin, cH, 125);  
-    beep(speakerPin, b, 125);  
-    beep(speakerPin, cH, 250);      
-    delay(250);
-    beep(speakerPin, f, 125);  
-    beep(speakerPin, gS, 500);  
-    beep(speakerPin, f, 375);  
-    beep(speakerPin, a, 125); 
-    
-    beep(speakerPin, cH, 500); 
-    beep(speakerPin, a, 375);  
-    beep(speakerPin, cH, 125); 
-    beep(speakerPin, eH, 1000); 
-    //more interesting stuff (this doesn't quite get it right somehow)
-    
-    beep(speakerPin, aH, 500);
-    beep(speakerPin, a, 350); 
-    beep(speakerPin, a, 150);
-    beep(speakerPin, aH, 500);
-    beep(speakerPin, gSH, 250); 
-    beep(speakerPin, gH, 250);
-    
-    beep(speakerPin, fSH, 125);
-    beep(speakerPin, fH, 125);    
-    beep(speakerPin, fSH, 250);
-    delay(250);
-    beep(speakerPin, aS, 250);    
-    beep(speakerPin, dSH, 500);  
-    beep(speakerPin, dH, 250);  
-    beep(speakerPin, cSH, 250);  
-    //repeat... repeat
-    
-    beep(speakerPin, cH, 125);  
-    beep(speakerPin, b, 125);  
-    beep(speakerPin, cH, 250);      
-    delay(250);
-    beep(speakerPin, f, 250);  
-    beep(speakerPin, gS, 500);  
-    beep(speakerPin, f, 375);  
-    beep(speakerPin, cH, 125); 
-           
-    beep(speakerPin, a, 500);            
-    beep(speakerPin, f, 375);            
-    beep(speakerPin, c, 125);            
-    beep(speakerPin, a, 1000);       
-    //and we're done \ó/    
+{
+  //for the sheet music see:
+  //http://www.musicnotes.com/sheetmusic/mtd.asp?ppn=MN0016254
+  //this is just a translation of said sheet music to frequencies / time in ms
+  //used 500 ms for a quart note
+
+  beep(speakerPin, a, 500);
+  beep(speakerPin, a, 500);
+  beep(speakerPin, a, 500);
+  beep(speakerPin, f, 350);
+  beep(speakerPin, cH, 150);
+
+  beep(speakerPin, a, 500);
+  beep(speakerPin, f, 350);
+  beep(speakerPin, cH, 150);
+  beep(speakerPin, a, 1000);
+  //first bit
+
+  beep(speakerPin, eH, 500);
+  beep(speakerPin, eH, 500);
+  beep(speakerPin, eH, 500);
+  beep(speakerPin, fH, 350);
+  beep(speakerPin, cH, 150);
+
+  beep(speakerPin, gS, 500);
+  beep(speakerPin, f, 350);
+  beep(speakerPin, cH, 150);
+  beep(speakerPin, a, 1000);
+  //second bit...
+
+  beep(speakerPin, aH, 500);
+  beep(speakerPin, a, 350);
+  beep(speakerPin, a, 150);
+  beep(speakerPin, aH, 500);
+  beep(speakerPin, gSH, 250);
+  beep(speakerPin, gH, 250);
+
+  beep(speakerPin, fSH, 125);
+  beep(speakerPin, fH, 125);
+  beep(speakerPin, fSH, 250);
+  delay(250);
+  beep(speakerPin, aS, 250);
+  beep(speakerPin, dSH, 500);
+  beep(speakerPin, dH, 250);
+  beep(speakerPin, cSH, 250);
+  //start of the interesting bit
+
+  beep(speakerPin, cH, 125);
+  beep(speakerPin, b, 125);
+  beep(speakerPin, cH, 250);
+  delay(250);
+  beep(speakerPin, f, 125);
+  beep(speakerPin, gS, 500);
+  beep(speakerPin, f, 375);
+  beep(speakerPin, a, 125);
+
+  beep(speakerPin, cH, 500);
+  beep(speakerPin, a, 375);
+  beep(speakerPin, cH, 125);
+  beep(speakerPin, eH, 1000);
+  //more interesting stuff (this doesn't quite get it right somehow)
+
+  beep(speakerPin, aH, 500);
+  beep(speakerPin, a, 350);
+  beep(speakerPin, a, 150);
+  beep(speakerPin, aH, 500);
+  beep(speakerPin, gSH, 250);
+  beep(speakerPin, gH, 250);
+
+  beep(speakerPin, fSH, 125);
+  beep(speakerPin, fH, 125);
+  beep(speakerPin, fSH, 250);
+  delay(250);
+  beep(speakerPin, aS, 250);
+  beep(speakerPin, dSH, 500);
+  beep(speakerPin, dH, 250);
+  beep(speakerPin, cSH, 250);
+  //repeat... repeat
+
+  beep(speakerPin, cH, 125);
+  beep(speakerPin, b, 125);
+  beep(speakerPin, cH, 250);
+  delay(250);
+  beep(speakerPin, f, 250);
+  beep(speakerPin, gS, 500);
+  beep(speakerPin, f, 375);
+  beep(speakerPin, cH, 125);
+
+  beep(speakerPin, a, 500);
+  beep(speakerPin, f, 375);
+  beep(speakerPin, c, 125);
+  beep(speakerPin, a, 1000);
+  //and we're done \ó/
 }
 void setup() {
-  noTone(13);
+  noTone(speakerPin);
   //set pins to output so you can control the shift register
   pinMode(latchPin, OUTPUT);
   pinMode(clockPin, OUTPUT);
@@ -178,48 +178,48 @@ void setup() {
 
 void loop() {
 
-  if(digitalRead(buttonPin) == HIGH) {
+  if (digitalRead(buttonPin) == HIGH) {
     light = !light;
     delay(500);
   }
 
-  if(light) {
-  
+  if (light) {
+
     int p = analogRead(p_pin);
     Serial.println(p);
-  
+
     int zapal = map(p, 0, 1024, 0, 9);
-  
+
     digitalWrite(latchPin, LOW);
     shiftOut(dataPin, clockPin, MSBFIRST, out[zapal]);
     digitalWrite(latchPin, HIGH);
     delay(1);
     off = false;
 
-    if(zapal == 8) {
+    if (zapal == 8) {
       march();
-      }
+    }
   }
-  else if(off == false){
+  else if (off == false) {
     digitalWrite(latchPin, LOW);
     shiftOut(dataPin, clockPin, MSBFIRST, out[0]);
     digitalWrite(latchPin, HIGH);
     off = true;
-    }
-  
-  
-  
-  // count from 0 to 255 and display the number 
+  }
+
+
+
+  // count from 0 to 255 and display the number
   // on the LEDs
   /*for (int numberToDisplay = 0; numberToDisplay < 256; numberToDisplay++) {
-    // take the latchPin low so 
+    // take the latchPin low so
     // the LEDs don't change while you're sending in bits:
     digitalWrite(latchPin, LOW);
     // shift out the bits:
-    shiftOut(dataPin, clockPin, MSBFIRST, numberToDisplay);  
+    shiftOut(dataPin, clockPin, MSBFIRST, numberToDisplay);
 
     //take the latch pin high so the LEDs will light up:
     digitalWrite(latchPin, HIGH);
     // pause before next value:*/
-    
+
 }
